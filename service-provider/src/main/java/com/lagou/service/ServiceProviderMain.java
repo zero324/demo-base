68f6b0b0e40528015836004beca0116f47b2f1ce
zero <272880214@qq.com>
2021-02-02 02:35:37 +0800

    first commit

---
+++ service-provider/src/main/java/com/lagou/service/ServiceProviderMain.java
@@ -0,0 +1,33 @@
+package com.lagou.service;
+
+import org.apache.dubbo.config.RegistryConfig;
+import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+import java.io.IOException;
+
+public class ServiceProviderMain {
+    public static void main(String[] args) throws IOException {
+        AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext(ServiceProviderConfigration.class);
+        applicationContext.start();
+        //System.in.read();
+        while(true){
+
+        }
+    }
+    @Configuration
+    @EnableDubbo(scanBasePackages="com.lagou.service.impl")
+    @PropertySource("classpath:/dubbo-provider.properties")
+    static class ServiceProviderConfigration{
+        @Bean
+        public RegistryConfig registryConfig(){
+            RegistryConfig registryConfig = new RegistryConfig();
+
+            registryConfig.setAddress("zookeeper://192.168.100.129:2181?timeout=10000");
+            return registryConfig;
+        }
+    }
+}
